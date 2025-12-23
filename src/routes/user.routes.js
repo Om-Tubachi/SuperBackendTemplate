e2526dcc05b063e1ccb212171b353e8ad9733176
Om-Tubachi <omtubachi06@gmail.com>
2025-12-23 22:20:20 +0530

    Completed user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,31 @@
 import Router from "express"
-import { registerUSer , loginUser , logOutUser ,refreshAccessToken} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+    registerUSer,
+    loginUser,
+    logOutUser,
+    refreshAccessToken,
+    getUserSubscribersAndSubscribedTo,
+    getWatchHistory,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAvatar,
+    updateAccountDetails,
+    updateUserCoverImage
+
+} 
+from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { verify } from "jsonwebtoken"
 const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
+            name: "avatar",
             maxCount: 1,
         },
         {
-            name:"coverImg",
+            name: "coverImg",
             maxCount: 1,
         }
     ]),
@@ -19,8 +34,48 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-the-token").post(refreshAccessToken)
-router.route("/").get((req , res , next) => {
-    res.json({message:"please use postman mister"})
+router.route("/profile/:username").get(getUserSubscribersAndSubscribedTo)
+router.route("/history").get(verifyJWT, getWatchHistory)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/user").get(verifyJWT, getCurrentUser)
+router.route("/change-avatar")
+    .patch(
+        verifyJWT,
+        upload.fields([
+            {
+                name: "avatar",
+                maxCount: 1,
+            }
+        ]),
+        updateUserAvatar
+    )
+router.route("/update-account-details").patch(
+    verifyJWT,
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImg",
+            maxCount: 1
+        }
+    ]),
+    updateAccountDetails
+)
+router.route("change-coverimage").patch(
+    verifyJWT,
+    upload.fields([
+        {
+            name: "coverImg",
+            maxCount: 1,
+        }
+    ]),
+    updateUserCoverImage
+)
+
+router.route("/").get((req, res, next) => {
+    res.json({ message: "please use postman mister" })
 })
 
 export default router
