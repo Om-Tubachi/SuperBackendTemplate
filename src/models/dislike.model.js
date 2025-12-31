9c7a65bbc8cc6e807364a96377e7be4a26cce300
Om-Tubachi <omtubachi06@gmail.com>
2025-12-31 14:11:32 +0530

    Added dislike model

---
+++ src/models/dislike.model.js
@@ -0,0 +1,27 @@
+import mongoose, {Schema} from "mongoose";
+
+const dislikeSchema = new Schema(
+    {
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: "Tweet"
+        },
+        dislikedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true    
+    }
+)
+
+export const Dislike = mongoose.model("Dislike" , dislikeSchema)
